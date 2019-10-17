e51c3dcf9d935ca38da8ce13bee330503c44fb03
NycollSM <nycollsotomorales21@gmail.com>
2019-10-16 20:51:53 -0600

    adding folders and pages

---
+++ src/pages/proxy.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="proxyOrg" />
+    <h1>Proxy Organizacion</h1>
+  </Layout>
+)
+
+export default IndexPage
