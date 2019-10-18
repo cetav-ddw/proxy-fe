cd9cfa85ee3ce1594f8aae1b367dd2b1cf91ba39
Gloriana Angulo Alvarado <g.angulo@b7-09.local>
2019-10-18 12:11:16 -0600

    Agrego la seccion de footer al index

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
+import Footer from '../sections/Footer/footer';
 
 const IndexPage = () => (
   <Layout>
     <SEO title="proxy" />
     <h1>Proxy 2019</h1>
+    <Footer />
   </Layout>
 );
 
