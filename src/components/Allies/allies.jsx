6ce16cd63937ae3a47d5f67a249015bbf4472e76
Gloriana Angulo Alvarado <g.angulo@b7-09.local>
2019-10-18 15:21:31 -0600

    Cambio ruta de imagenes

--- src/components/Allies/allies.jsx
+++ src/components/Allies/allies.jsx
@@ -4,11 +4,11 @@ import './allies.scss';
 const Allies = () => (
   <div className="allies">
     <h5 className="allies__title">Colaboradores</h5>
-    <img className="allies__item--size" src="TRIPARTITO-CETAV.png" alt="Ministerio de cultura y juventud,Parque la Libertad,CETAV Centro de Tecnología y Artes Visuales" />
-    <img className="allies__item" src="UMC-blanco.png" alt="UMC" />
-    <img className="allies__item" src="LOGO-TRIANGULO-blanco.png" alt="Triangulo Showroom Creative" />
-    <img className="allies__item" src="submarca-blanco2.png" alt="Cooperación Española" />
-    <img className="allies__item" src="logo-Telerad-blanco.png" alt="Telerad Telecomunicaciones Radiodigitales" />
+    <img className="allies__item--size" src="./footer/TRIPARTITO-CETAV.png" alt="Ministerio de cultura y juventud,Parque la Libertad,CETAV Centro de Tecnología y Artes Visuales" />
+    <img className="allies__item" src="./footer/UMC-blanco.png" alt="UMC" />
+    <img className="allies__item" src="./footer/LOGO-TRIANGULO-blanco.png" alt="Triangulo Showroom Creative" />
+    <img className="allies__item" src="./footer/submarca-blanco2.png" alt="Cooperación Española" />
+    <img className="allies__item" src="./footer/logo-Telerad-blanco.png" alt="Telerad Telecomunicaciones Radiodigitales" />
 
   </div>
 );
--- src/components/Media/media.jsx
+++ src/components/Media/media.jsx
@@ -4,10 +4,10 @@ import './media.scss';
 const Media = () => (
   <div className="media">
     <a className="media_link" href="https://www.instagram.com/proxy_hiperreal/?hl=es-la">
-      <img className="media__icon media__icon--mgl" src="insta.svg" alt="Instagram de Proxy" />
+      <img className="media__icon media__icon--mgl" src="./social/insta.svg" alt="Instagram de Proxy" />
     </a>
     <a className="media_link" href="https://www.facebook.com/proxy.hiperreal/">
-      <img className="media__icon" src="face.svg" alt="Facebook de Proxy" />
+      <img className="media__icon" src="./social/face.svg" alt="Facebook de Proxy" />
     </a>
   </div>
 );
