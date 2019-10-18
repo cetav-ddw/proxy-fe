37260611fd97c55031daadb219ed23eddf3fd726
jimmy alvarez <geovanniam171078@gmail.com>
2019-10-18 10:42:08 -0600

    update: exports

--- src/pages/sinestesia.jsx
+++ src/pages/sinestesia.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 
-const IndexPage = () => (
+const Sinestesia = () => (
   <Layout>
     <SEO title="sinestesia" />
     <h1>Sinestesia</h1>
   </Layout>
 );
 
-export default IndexPage;
+export default Sinestesia;
