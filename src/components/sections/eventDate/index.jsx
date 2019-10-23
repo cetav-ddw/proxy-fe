ff88a7af562535afc30c44200f1afe6802b9a49a
NycollSM <nycollsotomorales21@gmail.com>
2019-10-23 17:01:43 -0600

    eliminando componentes innecesarios

--- src/components/sections/eventDate/index.jsx
+++ src/components/sections/eventDate/index.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import Paragraph from '../../units/Paragraph';
 import './date.scss';
 
 const EventDate = () => (
   <div className="wrapper-animation">
-    <Paragraph variant="wrapper-animation__item">20 novimebre 2019 hasta marzo 2020 </Paragraph>
-    <Paragraph variant="wrapper-animation__item">20 novimebre 2019 hasta marzo 2020 </Paragraph>
-    <Paragraph variant="wrapper-animation__item">20 novimebre 2019 hasta marzo 2020 </Paragraph>
+    <p className="wrapper-animation__item">20 novimebre 2019 hasta marzo 2020 </p>
+    <p className="wrapper-animation__item">20 novimebre 2019 hasta marzo 2020 </p>
+    <p className="wrapper-animation__item">20 novimebre 2019 hasta marzo 2020 </p>
   </div>
 );
 
