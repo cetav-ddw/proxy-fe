f7686d678c085fceb6621d2f052da472c056f8e9
jimmy alvarez <geovanniam171078@gmail.com>
2019-10-24 19:36:19 -0600

    delete: styles work component

--- src/components/sections/workUnits/index.jsx
+++ src/components/sections/workUnits/index.jsx
@@ -7,21 +7,19 @@ import './workUnit.scss';
 
 const Work = ({ allWorks }) => {
   const node = allWorks.map((item) => item.node);
-  console.log(node, React);
-  const structure = allWorks.map((i, index) => {
-    console.log(i.node.images[0].url);
-    let modific = '';
+  let modific = '';
+  const structure = node.map((i, index) => {
     if (index === 1) modific = '--right';
     return (
       <>
         <div className="works__content-work">
           <div className={`wrapper-image${modific} wrapper-images`}>
-            <Image variant="wrapper-image__image" source={i.node.images[0].url} alt="arte" />
-            <h4 className={`wrapper-image__name${modific}`}>{i.node.firstName}</h4>
-            <h4 className="wrapper-image__name lastname">{i.node.secondName}</h4>
+            <Image variant="wrapper-image__image" source={i.images[0].url} alt="arte" />
+            <h4 className={`wrapper-image__name${modific}`}>{i.firstName}</h4>
+            <h4 className={`wrapper-image__name${modific} lastname`}>{i.secondName}</h4>
           </div>
-          <div className={`wrapper-text${modific}`}>
-            <p className={`wrapper-text__paragraph${modific}`}>{i.node.description}</p>
+          <div className={`wrapper-text wrapper-text${modific}`}>
+            <p className={`wrapper-text__paragraph${modific}`}>{i.description}</p>
             <Link to="/" className={`wrapper-text__details${modific} details-ui`}>mas detalles +</Link>
           </div>
         </div>
