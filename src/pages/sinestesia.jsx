1b4c368cdced2ed42cc510e9b8ea1b6c3e58c40c
NycollSM <nycollsotomorales21@gmail.com>
2019-10-25 12:59:59 -0600

    contenido

--- src/pages/sinestesia.jsx
+++ src/pages/sinestesia.jsx
@@ -1,9 +1,9 @@
-/* eslint-disable max-len */
 import React from 'react';
 import SEO from '../components/seo';
 import MainLayout from '../components/layouts/MainLayout';
 import SinestesiaD from '../components/sections/sinestesiaDesc';
 import '../scss/styles.scss';
+import '../scss/sinestesia.scss';
 
 const titlePage = 'Sinestesia';
 
@@ -18,17 +18,21 @@ const Sinestesia = () => (
       }
       content={(
         <div className="sinestesia-dates">
-          <p>Pronto más información</p>
+          <h2 className="sinestesia-dates__title">Fechas Importantes</h2>
+          <div className="wrapper__txt--columns">
+            <p className="txt--columns">Pronto más información</p>
+          </div>
           <h3 className="note__title">Nota</h3>
           <div className="wrapper__txt--columns">
             <p className="txt--columns">
-            // eslint-disable-next-line max-len
-          Brindaremos talleres para que las personas interesadas profundicen sobre temas de improvisación en música, teatro y danza así como live coding, visuales y sensores de movimiento.
+              Brindaremos talleres para que las personas interesadas profundicen sobre
+              temas de improvisación en música, teatro y
+              danza así como live coding, visuales y sensores de movimiento.
             </p>
             <p className="txt--columns">
               No es necesaria tener experiencia con el uso de este tipo de herramientas.
+              Solo la disposición a jugar o interactuar con estas.
             </p>
-            <p>Solo la disposición a jugar o interactuar con estas.</p>
           </div>
         </div>
       )}
