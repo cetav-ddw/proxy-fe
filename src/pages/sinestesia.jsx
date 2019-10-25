c1cc8d939dee1e2a130396816e8e144fd8fc76c6
NycollSM <nycollsotomorales21@gmail.com>
2019-10-25 11:46:39 -0600

    merge

--- src/pages/sinestesia.jsx
+++ src/pages/sinestesia.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 import React from 'react';
 import SEO from '../components/seo';
 import MainLayout from '../components/layouts/MainLayout';
@@ -18,7 +19,18 @@ const Sinestesia = () => (
       content={(
         <div className="sinestesia-dates">
           <h2 className="sinestesia-dates__title">Fechas Importantes</h2>
-          <p>requisitos</p>
+          <p>Pronto más información</p>
+          <h3 className="note__title">Nota</h3>
+          <div className="wrapper__txt--columns">
+            <p className="txt--columns">
+            // eslint-disable-next-line max-len
+          Brindaremos talleres para que las personas interesadas profundicen sobre temas de improvisación en música, teatro y danza así como live coding, visuales y sensores de movimiento.
+            </p>
+            <p className="txt--columns">
+              No es necesaria tener experiencia con el uso de este tipo de herramientas.
+            </p>
+            <p>Solo la disposición a jugar o interactuar con estas.</p>
+          </div>
         </div>
       )}
     />
