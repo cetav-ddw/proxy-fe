75b3499c9d099693bf62debf93772ff53e71a655
Jose Leiva <jdleiva@backcountry.com>
2019-10-25 18:40:39 -0600

    Feedback PR

--- src/components/sections/sinestesiaDesc/index.jsx
+++ src/components/sections/sinestesiaDesc/index.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable max-len */
 import React from 'react';
 import './sinestesia.scss';
 
@@ -7,11 +6,19 @@ const Sinestesia = () => (
     <h1 className="sinestesia__title">Sinestesia</h1>
     <div className="wrapper__des">
       <p>Es una serie de performances improvisados que integran multimedia.</p>
-      <p>Se juntan artistas de artes visuales, sonoras, literarias y del movimiento en escenarios intervenidos para generar un diálogo artístico interdisciplinario digno de la era digital.</p>
-      <p> Se da un tiempo determinado para realizar un performance improvisado que haga uso de herramientas tecnológicas (luces, video mapping, live coding, música electrónica, sensores, amplificación y efectos).</p>
+      <p>
+        Se juntan artistas de artes visuales, sonoras, literarias y del
+        movimiento en escenarios intervenidos para generar un diálogo artístico
+        interdisciplinario digno de la era digital.
+      </p>
+      <p>
+        {' '}
+        Se da un tiempo determinado para realizar un performance improvisado que
+        haga uso de herramientas tecnológicas (luces, video mapping, live
+        coding, música electrónica, sensores, amplificación y efectos).
+      </p>
     </div>
   </div>
 );
-
 
 export default Sinestesia;
