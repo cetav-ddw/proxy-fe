10f0986d1515bff785d88e3a16f3130cab51741c
Guayo Mena <guayo.mena@gmail.com>
2019-11-12 16:57:17 -0600

    Usar gatsby image en workGallery

--- src/components/sections/workGallery/index.jsx
+++ src/components/sections/workGallery/index.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import Image from '../../units/Image';
 import './workGallery.scss';
+import Img from 'gatsby-image';
 
-const WorkGallery = ({ mediaData }) => (
+const WorkGallery = ({ mediaData, title }) => (
   <section className="gallery">
     {mediaData.length > 0 && (
-      <div className="gallery__wrapper-img">
-        <Image variant="gallery__img-art" source={mediaData[0].url} alt="" />
+      <div
+        className="gallery__wrapper-img"
+      >
+        <Img fluid={mediaData[0].fluid} alt={title} />
       </div>
     )}
   </section>
